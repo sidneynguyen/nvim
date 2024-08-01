ca1775719bb13955f30049bf240b1c78a7745aa7
Sidney Nguyen <sidnguye@amazon.com>
2024-08-01 00:05:21 -0700

    add copy link keymap

--- lua/options.lua
+++ lua/options.lua
@@ -90,21 +90,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -118,3 +103,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Copy link to remote
+vim.keymap.set('n', '<leader>l', function()
+  local workingDir = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+  local relativePath = vim.fn.expand '%'
+
+  local brazilPath = os.getenv 'REMOTE_LINK_PREFIX' .. workingDir .. os.getenv 'REMOTE_LINK_BRANCH_SEPARATOR' .. relativePath
+
+  vim.fn.setreg('+', brazilPath)
+end, { desc = '' })
--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -309,7 +309,7 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
-        jq = {},
+        json = { 'jq' },
       },
     },
   },
