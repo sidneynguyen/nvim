8817d8d54bdcf01128c26e788db1f4057f370a7d
Sidney Nguyen <sidnguye@amazon.com>
2024-08-01 01:34:58 -0700

    add Rust LSP

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -164,8 +164,8 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -247,6 +247,7 @@ return {
         'vimdoc',
         'javascript',
         'typescript',
+        'rust',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
